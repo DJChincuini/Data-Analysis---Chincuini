be60d0466b0eca26bf396e9fbb9b33218e542d69
DJChincuini <daantechincuini42@gmail.com>
2024-05-03 20:00:49 -0300

    Creación de páginas

---
+++ streamlit/pages/2_ Sobre el Creador.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write("### Sobre mí")
+
+st.write(
+    "📌 Mi nombre es Dante. Soy un estudiante apasionado de programación con un enfoque especial en el diseño, desarrollo e implementación de soluciones robustas por medio del Data Engineering 💻 así cómo también de la búsqueda de insights a través de los datos y del pensamiento analítico a la hora de la toma de decisiones 📊."
+         )
+
+st.write(
+    "📈 Me considero experto en la creación de arquitecturas escalables para procesamiento gracias a mí conocimientos de GCP y Python, cómo también del análisis de datos ayudándome con mi conocimiento de SQL y de lenguaje Dax."
+         )
+
+st.write(
+    "💪 Siempre en busca de desafíos que impulsen la innovación y la eficiencia en el mundo de la tecnología, mí objetivo es fusionar es el poder colaborar con profesionales tan apasionados cómo yo en pos de impulsar la toma de decisiones basada en los datos."
+)
