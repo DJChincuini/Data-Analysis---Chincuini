241e00840a089f742b2503a60941d54955275846
DJChincuini <daantechincuini42@gmail.com>
2024-05-03 20:15:16 -0300

    main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -7,3 +7,11 @@ st.write("*Creado por Dante Chincuini*")
 st.write("##")
 
 st.write("##### **Introducción**")
+
+st.write(
+    "En este proyecto abordaré dos datasets facilitados por el Observatorio de Movilidad y Seguridad Vial de la ciudad autónoma de Buenos Aires. Mí función es analizar estos datos en pos de conseguir insights que ayudarán a entender el porqué de estos siniestros y factores de riesgos que ayudan a la multiplicación de nuevos casos."
+)
+
+st.write(
+    "A la izquierda de esta página se encuentra un índice por el que podrás acceder al Dashboard diseñado para este proyecto."
+)
