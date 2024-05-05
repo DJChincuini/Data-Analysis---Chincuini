1137bfa958d49fa65f39399d90f034c5da0ef979
DJChincuini <daantechincuini42@gmail.com>
2024-05-05 13:20:06 -0300

    streamlit

---
+++ streamlit/streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st 
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.write("### Siniestros viales en la ciudad de Buenos Aires")
+
+st.write("*Creado por Dante Chincuini*")
+st.write("##")
+
+st.write("##### **Introducción**")
+
+st.write(
+    '''
+    En este proyecto abordaré dos datasets facilitados por el Observatorio de Movilidad y Seguridad Vial de la ciudad autónoma de Buenos Aires. Mí función es analizar estos datos en pos de conseguir insights que ayudarán a entender el porqué de estos siniestros y factores de riesgos que ayudan a la multiplicación de nuevos casos.
+
+A la izquierda de esta página se encuentra el índice por el que podrás acceder al Dashboard diseñado para este proyecto.
+    '''
+)
